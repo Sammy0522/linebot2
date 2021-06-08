448eecd219657239930f0b0fe5cb09ade89db4bf
Sammy <nm3220548@gmail.com>
2021-06-08 17:12:15 +0800

    fix

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('FacASQSqS6h4tgSemSAks0dJ8KxjZbJcrsIfzSVzNBMZPk6hVGDg4YStJ1Qn26+VOv6/SSBM2A6FyklYK13ZvbZwv9U65GEmd/1TW+d65sqsOISbxdy+Oj+gMj5q//nbrSuMMAJMO8PsI1wgZzno+gdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('3a917f7188d6154a4652dc794923b849')
 
 
 @app.route("/callback", methods=['POST'])
@@ -89,3 +89,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run()
+    
